673cdab0de06177377d9079f61e877ca11041978
= <nelsonnnnn.junior@gmail.com>
2026-03-14 00:05:04 -0300

    3 primeiros exercícios resolvidos.

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
-
 void main(){
 
+
     Scanner sc = new Scanner(System.in);
+
+    int valor1, valor2, soma;
+
+    valor1 = sc.nextInt();
+    valor2 = sc.nextInt();
+    soma = valor1 + valor2;
+
+    IO.println("SOMA= " + soma);
 
 
     
